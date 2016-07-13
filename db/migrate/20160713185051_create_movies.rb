715a985a0ecad66d16158a0568210aef4b01c590
Susie O'Brien <email": null>
2016-07-13 13:57:42 -0500

    set up controller, model, and migrate files

---
+++ db/migrate/20160713185051_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title, null: false
+      t.string :description, null: false
+      t.string :director, null: false
+      t.date :release_date
+      t.timestamps(null: false)
+    end
+  end
+end
