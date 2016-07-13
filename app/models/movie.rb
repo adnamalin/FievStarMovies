54f59438c811184da23858e4be84a6efeb14b5d7
Susie O'Brien <email": null>
2016-07-13 14:50:12 -0500

    Add average rating to model and view

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,9 +2,15 @@ class Movie < ActiveRecord::Base
   has_many :reviews
 
   def average_rating
-    # total = 0
-    # self.reviews.each do |review|
-    #   review.rating
+    if number_of_reviews == 0
+      return 0
+    else
+      total = 0
+      self.reviews.each do |review|
+        total += review.rating
+      end
+      total/number_of_reviews
+    end
   end
 
   def number_of_reviews
