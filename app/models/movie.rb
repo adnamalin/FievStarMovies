91a813d6469e5b232c3ccafb2dfe4b06a70e9a03
Stephano Hong <email": null>
2016-07-14 09:26:41 -0500

    Small refactoring of movie model and make tests pass.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,18 +6,18 @@ class Movie < ActiveRecord::Base
   validates :description, :director, :release_date, presence: true
 
   def average_rating
-    if number_of_reviews == 0
+    if number_of_ratings == 0
       return 0
     else
       total = 0
       self.ratings.each do |rat|
         total += rat.rating
       end
-      total/number_of_reviews
+      total/number_of_ratings
     end
   end
 
-  def number_of_reviews
-    self.reviews.count
+  def number_of_ratings
+    self.ratings.count
   end
 end
