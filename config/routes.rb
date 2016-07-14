322bc21d35592fad4883eade27e443d404bfd9c1
viKuo <lost.angel.fin@gmail.com>
2016-07-13 21:06:32 -0500

    cleaned up routes page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
+  resources :users, only: [:new, :create]
 
   root 'movies#index'
-  get '/users/new' => 'users#new'
-  post '/users' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
