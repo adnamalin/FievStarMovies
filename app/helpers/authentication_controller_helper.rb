f6da70e42621f0387920b8d2ee906cd68ae1089c
Susie O'Brien <email": null>
2016-07-15 11:41:02 -0500

    Can't rate your own review but you can comment on your own review

--- app/helpers/authentication_controller_helper.rb
+++ app/helpers/authentication_controller_helper.rb
@@ -13,11 +13,4 @@ module AuthenticationControllerHelper
     redirect_to no_access_path unless logged_in?
   end
 
-  # def havent_left_review_yet(movie)
-  #   !movies_already_reviewed.include?(movie.id)
-  # end
-
-  # def movies_already_reviewed
-  #   current_user.reviews.map { |rev| rev.movie_id }
-  # end
 end
--- app/helpers/validation_helper.rb
+++ app/helpers/validation_helper.rb
@@ -8,4 +8,16 @@ module ValidationHelper
     current_user.reviews.map { |rev| rev.movie_id }
   end
 
+  def havent_rated_yet(rev)
+    !users_who_already_rated_this_review(rev).include?(current_user.id)
+  end
+
+  def users_who_already_rated_this_review(rev)
+    rev.ratings.map { |rat| rat.rater_id }
+  end
+
+  def not_your_own(obj)
+    !authorized?(obj)
+  end
+
 end
